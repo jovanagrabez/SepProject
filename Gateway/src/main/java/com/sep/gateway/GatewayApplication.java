822e4e18715af5ecb00aeb2d03753e789be537ef
nikolinastamenic <nikolinastamenic@uns.ac.rs>
2019-11-22 19:00:39 +0100

    Added zuul and eureka servers

--- Gateway/src/main/java/com/sep/gateway/GatewayApplication.java
+++ Gateway/src/main/java/com/sep/gateway/GatewayApplication.java
@@ -1,15 +1,17 @@
-package com.sep.nc;
+package com.sep.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@SpringBootApplication
+@EnableZuulProxy
 @EnableEurekaClient
-public class NcApplication {
+@SpringBootApplication
+public class GatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(NcApplication.class, args);
+        SpringApplication.run(GatewayApplication.class, args);
     }
 
 }
--- KP/src/main/java/com/sep/kp/KpApplication.java
+++ KP/src/main/java/com/sep/kp/KpApplication.java
@@ -2,8 +2,10 @@ package com.sep.kp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class KpApplication {
 
     public static void main(String[] args) {
