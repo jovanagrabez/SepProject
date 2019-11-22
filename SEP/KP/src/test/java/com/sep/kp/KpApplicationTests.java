ede85fb3b28478bf6aeddaec1e25afa1a8452755
nikolinastamenic <nikolinastamenic@uns.ac.rs>
2019-11-22 19:03:51 +0100

    folder fix

---
+++ SEP/KP/src/test/java/com/sep/kp/KpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sep.kp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
