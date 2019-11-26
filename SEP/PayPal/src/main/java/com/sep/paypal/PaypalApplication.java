05f4a34add43637c372b2f4c97218b92f7bf8d60
nikolinastamenic <nikolinastamenic@uns.ac.rs>
2019-11-26 14:34:19 +0100

    Small changes

--- SEP/PayPal/src/main/java/com/sep/paypal/PaypalApplication.java
+++ SEP/PayPal/src/main/java/com/sep/paypal/PaypalApplication.java
@@ -3,9 +3,11 @@ package com.sep.paypal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RibbonClient(name = "PayPal")
 public class PaypalApplication {
 
     public static void main(String[] args) {
