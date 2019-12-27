0a8eb063c61e0ddf4b9fd31301d09e0fb1d2c963
Jovana Grabez <jovanagrabez@uns.ac.rs>
2019-12-27 21:59:12 +0100

    front for bank

--- bank-frontend/src/app/component/home/card-component/card-component.component.ts
+++ bank-frontend/src/app/component/home/card-component/card-component.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {CardServiceService} from '../../../service/card-service.service';
 
 @Component({
   selector: 'app-card-component',
@@ -7,12 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponentComponent implements OnInit {
 
-  constructor() { }
+
+  private Id;
+  private data;
+  private nesto;
+
+  cardElements = {pan: '', securityCode: 0, cardHolderName: '', validTo: null, paymentId: 0,
+    amount: 0,
+    merchantOrderId: 0}
+  constructor(private router: Router, private route: ActivatedRoute, private cardService: CardServiceService) { }
 
   ngOnInit() {
+    this.Id = this.route.snapshot.paramMap.get('id');
+    this.cardService.getData(this.Id).subscribe(res => {
+      this.data = res;
+
+    });
+    console.log(this.data);
   }
 
   checkCard() {
+    // @ts-ignore
+    this.cardElements = {amount: this.data.amount , merchantOrderId: this.data.merchantOrderId, paymentId: this.data.paymentId};
+    this.cardService.submitData(this.cardElements).subscribe(res => {
+      this.nesto = res;
 
+    });
   }
 }
