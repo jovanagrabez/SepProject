26af2814ab307f496f214ed11fabfc97f09e0b98
Jovana Grabez <jovanagrabez@uns.ac.rs>
2019-12-28 04:14:59 +0100

    amount

--- bank-frontend/src/app/component/home/card-component/card-component.component.ts
+++ bank-frontend/src/app/component/home/card-component/card-component.component.ts
@@ -1,8 +1,8 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {CardServiceService} from '../../../service/card-service.service';
-import {DOCUMENT} from "@angular/common";
-import {ToastrManager} from "ng6-toastr-notifications";
+import {DOCUMENT} from '@angular/common';
+import {ToastrManager} from 'ng6-toastr-notifications';
 
 @Component({
   selector: 'app-card-component',
@@ -16,9 +16,9 @@ export class CardComponentComponent implements OnInit {
   private data;
   private nesto;
 
-  cardElements = {pan: '', securityCode: 0, cardHolderName: '', validTo: null, paymentId: 0,
+  cardElements = {sellerId: 0, pan: '', securityCode: 0, cardHolderName: '', validTo: null, paymentId: 0,
     amount: 0,
-    merchantOrderId: 0, hashedId: 0}
+    merchantOrderId: 0, hashedId: 0};
   constructor(private router: Router, private route: ActivatedRoute, private cardService: CardServiceService,
               @Inject(DOCUMENT) private document: Document, private toastr: ToastrManager) { }
 
@@ -37,16 +37,17 @@ export class CardComponentComponent implements OnInit {
     this.cardElements.merchantOrderId = this.data.merchantOrderId;
     this.cardElements.paymentId = this.data.paymentId;
     this.cardElements.hashedId = this.Id;
+    this.cardElements.sellerId = this.data.sellerId;
 
     this.cardService.submitData(this.cardElements).subscribe(res => {
-      if (res['status'] === 'SUCCESS') {
+      if (res.status === 'SUCCESS') {
         this.toastr.successToastr('Successful transaction!');
       } else {
         this.toastr.warningToastr('Error in transaction!');
       }
       this.nesto = res;
       // @ts-ignore
-      this.document.location.href = res['url'];
+      this.document.location.href = res.url;
 
     });
   }
