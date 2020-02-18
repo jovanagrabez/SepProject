59c0eea2f618018cc07c6b929c86d308e0fba125
nikolinastamenic <nikolinastamenic@uns.ac.rs>
2020-02-18 23:55:28 +0100

    Register users

--- SEP/NC/src/main/java/com/sep/nc/common/PasswordChecker.java
+++ SEP/NC/src/main/java/com/sep/nc/common/PasswordChecker.java
@@ -1,30 +1,32 @@
 package com.sep.nc.common;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 public class PasswordChecker {
 
 
-	public static boolean check10000FrequentlyUsed(String pass) throws IOException {
+	public static boolean check10000FrequentlyUsed(String pass) {
 		/*
 		 * Provjera da li je password medju 10000 najkoriscenijih sifara
 		 */
-		File file = new File("src/main/resources/passwords.txt");
+		File file = new File("C:\\Users\\Laptop\\Nina\\Fakultet\\Master\\SEP\\Projekat\\SEP\\SEP\\NC\\src\\main\\resources\\passwords.txt");
 
-		BufferedReader br = new BufferedReader(new FileReader(file));
-
-		String st;
-		while ((st = br.readLine()) != null) {
-			System.out.println(st);
-			if(st.equals(pass)) {
-				return false;
+		BufferedReader br = null;
+		try {
+			br = new BufferedReader(new FileReader(file));
+			String st;
+			while ((st = br.readLine()) != null) {
+				System.out.println(st);
+				if(st.equals(pass)) {
+					return false;
+				}
 			}
+
+			return true;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
 		}
-		
-		return true;
 	}
 
 
